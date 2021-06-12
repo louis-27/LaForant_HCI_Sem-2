1b6f62eef396d49ed654617ca2d0bc1b35195496
“louis-27” <louis27isnow@gmail.com>
2021-06-12 18:02:52 +0700

    Fourth Progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,35 @@ let toggleNavDropdown = () => {
 }
 
 // Image Slider (JQuery)
+$(function () {
+    let slides = $('.agent-image');
+    slides.hide();
+
+    let currId = 0;
+    let minId = 0;
+    let maxId = slides.length - 1;
+
+    slides.eq(0).show();
+
+    let rightArrow = $('#slide-right');
+    let leftArrow = $('#slide-left');
+
+    rightArrow.click(() => {
+        slides.eq(currId).hide();
+        currId++;
+        if (currId > maxId) {
+            currId = minId;
+        }
+        slides.eq(currId).show();
+    })
+
+    leftArrow.click(() => {
+        slides.eq(currId).hide();
+        currId--;
+        if (currId < minId) {
+            currId = maxId;
+        }
+        slides.eq(currId).show();
+    })
+
+})
