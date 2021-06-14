93a0484700f980d45adb750b255fa46910c1265f
“louis-27” <louis27isnow@gmail.com>
2021-06-14 17:35:01 +0700

    Homepage almost done (Not responsive)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 let toggleState = false;
 
 let toggleNavDropdown = () => {
-    let element = document.getElementById('elementId');
+    let element = document.getElementById('nav-menu');
 
     if (toggleState == true) {
         element.style.display = 'none';
@@ -18,31 +18,39 @@ $(function () {
     let slides = $('.agent-image');
     slides.hide();
 
+    let agentDesc = $('.agents-desc');
+    agentDesc.hide();
+
     let currId = 0;
     let minId = 0;
     let maxId = slides.length - 1;
 
     slides.eq(0).show();
+    agentDesc.eq(0).show();
 
     let rightArrow = $('#slide-right');
     let leftArrow = $('#slide-left');
 
     rightArrow.click(() => {
         slides.eq(currId).hide();
+        agentDesc.eq(currId).hide();
         currId++;
         if (currId > maxId) {
             currId = minId;
         }
         slides.eq(currId).show();
+        agentDesc.eq(currId).show();
     })
 
     leftArrow.click(() => {
         slides.eq(currId).hide();
+        agentDesc.eq(currId).hide();
         currId--;
         if (currId < minId) {
             currId = maxId;
         }
         slides.eq(currId).show();
+        agentDesc.eq(currId).show();
     })
 
 })
